b5882760872fe19bf9076e20eda650b4f433451f
arttttt <artem-bambalov@yandex.ru>
2024-09-12 02:45:27 +0300

    empty kmm module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "SimpleMVI"
 include(":app")
+include(":simplemvi")
---
+++ simplemvi/build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinAndroidTarget
+import org.jetbrains.kotlin.gradle.targets.jvm.KotlinJvmTarget
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
+}
+
+kotlin {
+    androidTarget()
+
+    targets.configureEach {
+        when (this) {
+            is KotlinAndroidTarget ->
+                compilerOptions {
+                    jvmTarget.set(JvmTarget.JVM_1_8)
+                }
+
+            is KotlinJvmTarget ->
+                compilerOptions {
+                    jvmTarget.set(JvmTarget.JVM_11)
+                }
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlin.atomicfu)
+            implementation(libs.kotlin.coroutines.core)
+        }
+    }
+}
+
+android {
+    namespace = "com.arttttt.simplemvi"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 24
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
