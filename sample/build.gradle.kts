67cdc6c9b56d01e9633cb62924970bce7be73355
arttttt <artem-bambalov@yandex.ru>
2024-09-12 02:55:53 +0300

    connect simplemvi module to sample

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -42,6 +42,8 @@ android {
 
 dependencies {
 
+    implementation(project(":simplemvi"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
