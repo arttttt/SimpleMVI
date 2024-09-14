63de5369ca6d629cfc2af19bac7e87768a81aad4
arttttt <artem-bambalov@yandex.ru>
2024-09-14 03:08:45 +0300

    add ksp

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.google.ksp)
 }
 
 android {
