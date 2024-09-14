4c1fb79616c0ef5e5f08bcb15e40cdd618ebbdd4
arttttt <artem-bambalov@yandex.ru>
2024-09-14 18:39:44 +0300

    enable edge to edge

--- sample/src/main/java/com/arttttt/simplemvi/MainActivity.kt
+++ sample/src/main/java/com/arttttt/simplemvi/MainActivity.kt
@@ -3,6 +3,7 @@ package com.arttttt.simplemvi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -12,6 +13,8 @@ import com.arttttt.simplemvi.ui.theme.SimpleMVITheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
 
         setContent {
             SimpleMVITheme {
