38df77885dedf3cf3cbac68fd914a06e736adfce
arttttt <artem-bambalov@yandex.ru>
2024-09-20 00:48:28 +0300

    fix ios compilation

--- simplemvi/src/iosMain/kotlin/com/arttttt/simplemvi/utils/MainThreadAssert.ios.kt
+++ simplemvi/src/iosMain/kotlin/com/arttttt/simplemvi/utils/MainThreadAssert.ios.kt
@@ -2,6 +2,6 @@ package com.arttttt.simplemvi.utils
 
 import platform.Foundation.NSThread
 
-actual fun isMainThread(): Boolean {
+public actual fun isMainThread(): Boolean {
     return NSThread.isMainThread
 }
