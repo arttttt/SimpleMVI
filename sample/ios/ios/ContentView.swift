788f80b03390c32791e34728f139b5d58530cb1f
arttttt <artem-bambalov@yandex.ru>
2024-09-22 00:30:54 +0300

    basic iOS project

---
+++ sample/ios/ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleMVISampleIOS
+//
+//  Created by Artem Bambalov on 22/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sample/ios/ios/SimpleMVISampleIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMVISampleIOSApp.swift
+//  SimpleMVISampleIOS
+//
+//  Created by Artem Bambalov on 22/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMVISampleIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
