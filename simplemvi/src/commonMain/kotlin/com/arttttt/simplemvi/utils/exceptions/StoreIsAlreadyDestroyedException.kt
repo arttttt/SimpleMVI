259be1a19ce1263c30ee172edf291d50d5d24d16
arttttt <artem-bambalov@yandex.ru>
2024-10-20 20:11:27 +0300

    update error messages

--- simplemvi/src/commonMain/kotlin/com/arttttt/simplemvi/utils/exceptions/StoreIsAlreadyDestroyedException.kt
+++ simplemvi/src/commonMain/kotlin/com/arttttt/simplemvi/utils/exceptions/StoreIsAlreadyDestroyedException.kt
@@ -1,3 +1,3 @@
 package com.arttttt.simplemvi.utils.exceptions
 
-public class StoreIsAlreadyDestroyedException : Exception("Attempting to use destroyed store")
+public class StoreIsAlreadyDestroyedException : Exception("Attempting to use a destroyed Store")
