86e996ce4fc32175e919cf93417c03c30d8ec134
arttttt <artem-bambalov@yandex.ru>
2024-10-20 20:06:54 +0300

    more robust error message

--- simplemvi/src/commonMain/kotlin/com/arttttt/simplemvi/utils/exceptions/StoreIsAlreadyDestroyedException.kt
+++ simplemvi/src/commonMain/kotlin/com/arttttt/simplemvi/utils/exceptions/StoreIsAlreadyDestroyedException.kt
@@ -1,3 +1,3 @@
 package com.arttttt.simplemvi.utils.exceptions
 
-public class StoreIsAlreadyDestroyedException : Exception("Attempting to call functions of the destroyed store")
+public class StoreIsAlreadyDestroyedException : Exception("Attempting to use destroyed store")
