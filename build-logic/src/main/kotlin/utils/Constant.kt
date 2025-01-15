d24a10019638a5309447bf6876215959ea4c58de
arttttt <artem-bambalov@yandex.ru>
2025-01-15 22:34:34 +0200

    bump up android target version

--- build-logic/src/main/kotlin/utils/Constant.kt
+++ build-logic/src/main/kotlin/utils/Constant.kt
@@ -1,6 +1,6 @@
 package utils
 
-const val COMPILE_SDK_VERSION = 34
+const val COMPILE_SDK_VERSION = 35
 const val MIN_SDK_VERSION = 24
 
 const val LIBRARY_VERSION = "0.4.0"
--- sample/android/build.gradle.kts
+++ sample/android/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.arttttt.simplemvi.sample"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.arttttt.simplemvi"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
