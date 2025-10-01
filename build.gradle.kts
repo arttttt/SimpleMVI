51e36cf8d55e7f397446da97cf27445a40b41921
arttttt <artem-bambalov@yandex.ru>
2025-10-02 01:10:02 +0300

    kotlin jvm dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.google.ksp) apply false
