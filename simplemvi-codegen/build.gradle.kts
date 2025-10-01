c547c8507b6c081aa503f465b4d225d9de31aca3
arttttt <artem-bambalov@yandex.ru>
2025-10-02 01:45:42 +0300

    set correct jvm target

--- simplemvi-codegen/build.gradle.kts
+++ simplemvi-codegen/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.google.ksp)
@@ -6,6 +8,12 @@ plugins {
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
 }
 
 dependencies {
