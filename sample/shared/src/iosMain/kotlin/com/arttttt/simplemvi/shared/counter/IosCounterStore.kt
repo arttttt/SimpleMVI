bdd864b01bb0f5fd616bc832eff3f3b645377f7e
arttttt <artem-bambalov@yandex.ru>
2025-10-15 01:24:51 +0300

    iOS extensions for stores

---
+++ sample/shared/src/iosMain/kotlin/com/arttttt/simplemvi/shared/counter/IosCounterStore.kt
@@ -0,0 +1,9 @@
+package com.arttttt.simplemvi.shared.counter
+
+import com.arttttt.simplemvi.logging.logger.com.arttttt.simplemvi.utils.IosStore
+import com.arttttt.simplemvi.logging.logger.com.arttttt.simplemvi.utils.asIosStore
+import com.arttttt.simplemvi.sample.shared.counter.CounterStore
+
+fun CounterStore.asIosStore(): IosStore<CounterStore.Intent, CounterStore.State, CounterStore.SideEffect> {
+    return this@asIosStore.asIosStore()
+}
---
+++ sample/shared/src/iosMain/kotlin/com/arttttt/simplemvi/shared/timer/IosTimerStore.kt
@@ -0,0 +1,9 @@
+package com.arttttt.simplemvi.shared.timer
+
+import com.arttttt.simplemvi.logging.logger.com.arttttt.simplemvi.utils.IosStore
+import com.arttttt.simplemvi.logging.logger.com.arttttt.simplemvi.utils.asIosStore
+import com.arttttt.simplemvi.sample.shared.timer.TimerStore
+
+fun TimerStore.asIosStore(): IosStore<TimerStore.Intent, TimerStore.State, TimerStore.SideEffect> {
+    return this@asIosStore.asIosStore()
+}
